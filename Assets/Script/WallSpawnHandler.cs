bb64826901c9bb5a04221e09efc23de7214473dd
Jaikishore <jaikishore@dlearners.in>
2023-02-12 23:10:59 +0530

    wall spawn optimisation by looking current position

--- Assets/Script/WallSpawnHandler.cs
+++ Assets/Script/WallSpawnHandler.cs
@@ -7,11 +7,11 @@ public class WallSpawnHandler : MonoBehaviour
     [SerializeField] Transform playerTransform;
     [SerializeField] GameObject[] walls;
     List<GameObject> childWalls = new List<GameObject>();
-    GameObject lastElement;
+    GameObject lastElement, spawnObject;
+    int currentPositionY;
 
     void Start()
     {
-        Debug.Log(gameObject.name+" Child Count : "+gameObject.transform.childCount);
         for(int i = 0; i < gameObject.transform.childCount; i++){
             childWalls.Add(gameObject.transform.GetChild(i).gameObject);
         }
@@ -19,21 +19,37 @@ public class WallSpawnHandler : MonoBehaviour
 
     void Update()
     {
-        if(((int)playerTransform.position.y % 10) == 0){
-            lastElement = childWalls[childWalls.Count - 1];
-            GameObject wall = Instantiate(
-                walls[Random.Range(0,walls.Length)], 
-                new Vector3(
-                    lastElement.transform.position.x, 
-                    lastElement.transform.position.y + 10, 
-                    lastElement.transform.position.z
-                ), 
-                Quaternion.identity
-            );
+        if(((int)playerTransform.position.y % 10) == 0 && currentPositionY != (int)playerTransform.position.y){
+            currentPositionY = (int)playerTransform.position.y;
+            lastElement = this.childWalls[this.childWalls.Count - 1];
+            spawnObject = walls[Random.Range(0,walls.Length)];
+            GameObject wall = GameController.instance.GetGameObjectInPool(spawnObject.tag);
+            
+            if(wall == null){
+                wall = Instantiate(
+                    spawnObject,
+                    new Vector3(
+                        lastElement.transform.position.x, 
+                        lastElement.transform.position.y + 10, 
+                        lastElement.transform.position.z
+                    ), 
+                    Quaternion.identity
+                );
+            }
 
             wall.transform.SetParent(transform);
 
-            childWalls.Add(wall);
+            this.childWalls.Add(wall);
+
+            Destroy(gameObject.transform.GetChild(0).gameObject);
+            // if(lastElement.tag == "RedWall"){
+            //     GameController.instance.redPooledGO.Add(lastElement);
+            // }else{
+            //     GameController.instance.bluePooledGO.Add(lastElement);
+            // }
+            // this.childWalls[0].SetActive(false);
+            // // this.childWalls[0].transform.parent = null;
+            // this.childWalls.RemoveAt(0);
         }
     }
 }
