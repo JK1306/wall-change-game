5aa73c646e12f2733c519a719736261be2ec8ce0
Jaikishore <jaikishore@dlearners.in>
2023-02-19 15:37:22 +0530

    object pooling for obstacles impelemented

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -11,40 +11,27 @@ public class GameController : MonoBehaviour
     [Header("ObstacleHandler")]
     [SerializeField] Transform[] obstacleSpawners;
     [SerializeField] GameObject obstacleSpawnPrefab;
+    [SerializeField] ObjectPooling objectPooling;
+    List<GameObject> spawnedObstacles;
     float timer;
+
+    [Header("SFX")]
+    [SerializeField] AudioClip deathSfx;
 
     void Start()
     {
         instance = this;
         redPooledGO = new List<GameObject>();
         bluePooledGO = new List<GameObject>();
+        spawnedObstacles = new List<GameObject>();
 
         SetTimer();
     }
 
     private void Update() {
         ObstacleSpawnTimer();
+        DisableObstacles();
     }
-
-#region Object Pooling
-    public GameObject GetGameObjectInPool(string wallTagName){
-        GameObject returnGameObject = null;
- 
-        if(wallTagName == "RedWall" && redPooledGO.Count > 0){
-            returnGameObject = redPooledGO[0];
-            returnGameObject.SetActive(true);
-            redPooledGO.RemoveAt(0);
-            return returnGameObject;
-        }else if(wallTagName == "BlueWall" && bluePooledGO.Count > 0){
-            returnGameObject = bluePooledGO[0];
-            returnGameObject.SetActive(true);
-            bluePooledGO.RemoveAt(0);
-            return returnGameObject;
-        }
- 
-        return returnGameObject;
-    }
-#endregion
 
 #region ObstacleHandler
     void SetTimer(){
@@ -53,12 +40,36 @@ public class GameController : MonoBehaviour
 
     void ObstacleSpawnTimer(){
         if(timer <= 0){
-            Instantiate(obstacleSpawnPrefab, obstacleSpawners[Random.Range(0, obstacleSpawners.Length)].position, Quaternion.identity);
+            GameObject spawnedObstacle;
+            spawnedObstacle = objectPooling.GetObjectFromPool(obstacleSpawnPrefab);
+            spawnedObstacle.SetActive(true);
+            spawnedObstacle.transform.position = obstacleSpawners[Random.Range(0, obstacleSpawners.Length)].position;
+            spawnedObstacles.Add(
+                spawnedObstacle
+                // (GameObject)Instantiate(obstacleSpawnPrefab, obstacleSpawners[Random.Range(0, obstacleSpawners.Length)].position, Quaternion.identity)
+            );
             timer = Random.Range(3, 8);
         }else{
             timer -= Time.deltaTime;
         }
     }
+
+    void DisableObstacles(){
+        if(spawnedObstacles.Count > 0){
+            for (int i=0; i<spawnedObstacles.Count; i++)
+            {
+                if(Vector3.Distance(spawnedObstacles[i].transform.position, obstacleSpawners[0].position) >= 10f){
+                    objectPooling.AddToPool(spawnedObstacles[i]);
+                    spawnedObstacles.RemoveAt(i);
+                }
+            }
+        }
+    }
 #endregion
+
+    public void PlayDeathSFX(){
+        gameObject.GetComponent<AudioSource>().clip = deathSfx;
+        gameObject.GetComponent<AudioSource>().Play();
+    }
 
 }
