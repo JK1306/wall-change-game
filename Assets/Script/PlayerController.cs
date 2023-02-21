e0cf546974ddc7d42233372ca0225ca3740c6a70
Jaikishore <jaikishore@dlearners.in>
2023-02-22 01:03:56 +0530

    game mechanics improved

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     public MOVERIGHT moveRight;
     public MOVELEFT moveLEFT;
     public static PlayerController instance;
-    public 
+    public bool playerIsAlive;
 
     void Start()
     {
@@ -23,6 +23,7 @@ public class PlayerController : MonoBehaviour
         moveRight  = MoveRight;
         moveLEFT = MoveLeft;
         movingDirection = Vector3.up;
+        playerIsAlive = true;
     }
 
     void FixedUpdate()
@@ -41,10 +42,12 @@ public class PlayerController : MonoBehaviour
 
     void MoveRight(){
         movingDirection = Vector3.right;
+        // Debug.Log("Direction : "+movingDirection);
     }
 
     void MoveLeft(){
         movingDirection = Vector3.left;
+        // Debug.Log("Direction : "+movingDirection);
     }
 
     public void ResetPosition(Vector3 movementDirection){
@@ -53,18 +56,24 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void IncreseMovementSpeed(float speedIncreaseRate){
+        movementSpeed += speedIncreaseRate;
+    }
+
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Enemy"){
+            playerIsAlive = false;
             gameObject.GetComponent<MeshRenderer>().enabled = false;
             movementSpeed = 0f;
             GameController.instance.PlayDeathSFX();
             playerDeathBurst.Play();
             StartCoroutine(DestroyDeathParticle());
+            GameController.instance.GameOver();
         }
     }
 
     IEnumerator DestroyDeathParticle(){
-        yield return new WaitForSeconds(10);
+        yield return new WaitForSeconds(5);
         Destroy(playerDeathBurst.gameObject);
     }
 
